6a6064db8235c2b338e17a5572dfba6bfcc0f8f2
Leo Pham <hongthaipro@gmail.com>
2019-03-31 22:38:11 +0700

    Updates

---
+++ precache-manifest.e978e6bc02b989e3f9f248f5406670e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18619767dd570637f712",
+    "url": "/react-the-movie-search/static/js/runtime~main.2b86a7eb.js"
+  },
+  {
+    "revision": "41d49bee5e5ea4f01c73",
+    "url": "/react-the-movie-search/static/js/main.19a3d83a.chunk.js"
+  },
+  {
+    "revision": "9dc38996a09368ad8623",
+    "url": "/react-the-movie-search/static/js/2.dc867659.chunk.js"
+  },
+  {
+    "revision": "41d49bee5e5ea4f01c73",
+    "url": "/react-the-movie-search/static/css/main.f995f87f.chunk.css"
+  },
+  {
+    "revision": "f5f05980511bb9d164c73552895deb86",
+    "url": "/react-the-movie-search/index.html"
+  }
+];
